d6412c2972a59498630dd280d2f9452b8a410b27
Alyaman Maasarani <alyaman.x.p300@gmail.com>
2022-02-05 17:46:41 +0200

    initial configuration

--- config.js
+++ config.js
@@ -11,26 +11,28 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Alyaman',
   imageBackground: false,
-  openInNewTab: true,
+  openInNewTab: false,
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: 'Ohayou gozaimasu!',
+  greetingAfternoon: 'Kon\'nichiwa,',
+  greetingEvening: 'Konbanwa,',
+  greetingNight: 'Oyasumi nasai!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherKey: '63383645b2ac94ac5534247c87ac9b5d', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
+  weatherLatitude: '34.4333',
+  weatherLongitude: '35.85',
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
-  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  // trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '34.4333',
+  defaultLongitude: '35.85',
 
   // Autochange
   autoChangeTheme: true,
@@ -39,7 +41,7 @@ const CONFIG = {
   changeThemeByOS: false, 
 
   // Autochange by hour options (24hrs format, string must be in: hh:mm)
-  changeThemeByHour: true,
+  changeThemeByHour: false,
   hourDarkThemeActive: '18:30',
   hourDarkThemeInactive: '07:00',
 
@@ -59,13 +61,13 @@ const CONFIG = {
       id: '2',
       name: 'Mail',
       icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      link: 'https://mail.google.com/',
     },
     {
       id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://todoist.com',
+      name: 'Moodle',
+      icon: 'book',
+      link: 'https://moodle.bau.edu.lb',
     },
     {
       id: '4',
@@ -75,15 +77,15 @@ const CONFIG = {
     },
     {
       id: '5',
-      name: 'Reddit',
-      icon: 'glasses',
-      link: 'https://reddit.com',
+      name: 'YouTube',
+      icon: 'youtube',
+      link: 'https://youtube.com',
     },
     {
       id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
+      name: 'Outlook',
+      icon: 'pen-tool',
+      link: 'https://outlook.office.com/',
     },
   ],
 
@@ -92,45 +94,45 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+  firstListIcon: 'globe',
+  secondListIcon: 'code',
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'NixOS Search',
+        link: 'https://search.nixos.org/',
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'HoYoLab',
+        link: 'https://hoyolab.com/home',
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'GitLab',
+        link: 'https://gitlab.com',
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Iconnect',
+        link: 'https://iconnect.bau.edu.lb',
       },
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: 'Rust Book',
+        link: 'https://doc.rust-lang.org/book/',
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: 'Rust By Example',
+        link: 'https://doc.rust-lang.org/rust-by-example/',
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: 'Rust Cookbook',
+        link: 'https://rust-lang-nursery.github.io/rust-cookbook/',
       },
       {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: 'CLI In Rust',
+        link: 'https://rust-cli.github.io/book/',
       },
     ],
   },
